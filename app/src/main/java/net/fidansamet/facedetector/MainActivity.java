283952bbbbbcb2d4009218177144e945b49d368c
fidansamet <fidansamet@hacettepe.edu.tr>
2019-02-23 14:49:04 +0300

    Ready for database

--- app/src/main/java/net/fidansamet/facedetector/MainActivity.java
+++ app/src/main/java/net/fidansamet/facedetector/MainActivity.java
@@ -1,6 +1,5 @@
 package net.fidansamet.facedetector;
 
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
